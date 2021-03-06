1af386a5852c40e7fe1dda72bce315e04df4b9fb
sangnd-fabbi <sangnd@fabbi.io>
2021-03-06 21:02:17 +0700

    update

--- node-persist.js
+++ node-persist.js
@@ -3,7 +3,7 @@ var storage = require('node-persist');
 
 // The function initialization
 // Load data has been saved in a disk
-storage.initSysc({
+storage.initSync({
     dir :"students" //Configure where the data is stored in the students directory
 });
 
@@ -45,23 +45,59 @@ function getStudent(studentId)
 //The function adds a student
 function addStudent(id, fullname)
 {
-    
+    var students = getAllStudents();
+
+    students.push({ 
+        id : id,
+        fullname : fullname
+    });
+
+    storage.setItemSync('students', students);
 }
 
 //The function removes a student
 function editStudent(studentId, studentName)
 {
+    var students = getAllStudents();
 
+    for (var i = 0; i < students.lenghtl; i++){
+        if (students[i].id === studentId){
+            students.splice(i, 1);
+        }
+    }
+    
+    storage.setItemSync('students', students);
 }
 
 //The function edit a student
 function editStudent(studentId, studentName)
 {
+    var students = getAllStudents();
+    
+    for (var i = 0; i < students.lengh; i++){
+        if (students[i].id === studentId){
+            students[i].fullname = studentName;
+        }
+    }
 
+    storage.setItemSync('students', students);
 }
 
 //The function displays a list of students
 function showStudents()
 {
-
+    var students = getAllStudents();
+    students.forEach(function(student){
+        console.log('Student: ' + student.fullname + ' (' + student.id + ')');
+    });
 }
+
+
+//Add new students
+addStudent(1, 'Linh');
+addStudent(2, 'Anh');
+addStudent(3, 'Giang');
+addStudent(4, 'Hoa');
+
+//Show list students
+showStudents();
